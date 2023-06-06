b5e8d0a1b28f695cf7c32b38352628b2782743c1
konditi1 <konditi1@gmail.com>
2023-06-06 08:45:04 -0700

    prints combination of two digits without repeating

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# print(", ".join("{}{}".format(i, j) for i in range(10) for j in range(1, 10)
-# if i < j))
 for i in range(10):
     for j in range(i + 1, 10):
         print("{:d}{:d}".format(i, j), end="")
