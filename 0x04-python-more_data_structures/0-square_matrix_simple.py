2306623c7b94af079e6c90dfeb9550b7b23035e9
konditi1 <konditi1@gmail.com>
2023-06-14 11:38:00 -0700

    squring the elements of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,6 @@ def square_matrix_simple(matrix=None):
         matrix = []
     changed_matrix = [row[:] for row in matrix]
     for row in range(len(changed_matrix)):
-        changed_matrix[row] = list(map(lambda item: item ** 2, changed_matrix[row]))
+        changed_matrix[row] = list(map(lambda item: item ** 2,
+                                   changed_matrix[row]))
     return changed_matrix
