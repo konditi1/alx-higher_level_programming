d1c242178f23fafc3208d72489303043bc17b2db
konditi1 <konditi1@gmail.com>
2023-06-26 16:11:36 -0700

    a function that prints an integer.

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        if value is None:
+            raise TypeError
+        print("{:d}".format(value))
+    except (ValueError, TypeError, NameError) as err:
+        error_message = "Exception: {}".format(err)
+        sys.stderr.write(error_message + '\n')
+        return False
+    else:
+        return True
