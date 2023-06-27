f84c43aebb960ad9b750caa40437c5b69db3c6ff
konditi1 <konditi1@gmail.com>
2023-06-27 12:20:49 -0700

    added index and value error hundling

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""A class that defines a square."""
+
+
+class Square:
+    """Represents a square.
+
+    A square is four-sided polygon with equal sides& right angles between them.
+    This class provides a basic representation of a square.
+
+    Attributes:
+        __size (int): The size (length of the side) of the square.
+
+    Methods:
+        __init__(self, size): Constructor for the Square class.
+    """
+
+    def __init__(self, size=0):
+        """Constructor for the Square class.
+
+        Initializes a new instance of the Square class with a given size.
+
+        Parameters:
+            size (int): The length of the side of the square. Default is 0.
+
+        Raises:
+            TypeError: If the size parameter is not an integer.
+            ValueError: If the size parameter is less than 0.
+
+        Returns:
+            None
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
