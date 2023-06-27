3a262e7231f6982763035c7f47c49a174b6e0081
konditi1 <konditi1@gmail.com>
2023-06-27 11:39:10 -0700

    added more comments

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python3
 class Square:
-    """Represents a square."""
+    """
+    Represents a square.
+
+    This class provides a basic representation of a square.
+
+    Attributes:
+        None
+
+    Methods:
+        __init__(): Constructor for the Square class.
+    """
 
     def __init__(self):
-        """Constructor for Square class."""
+        """
+        Constructor for Square class.
+
+        Initializes a new instance of the Square class.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         pass
+
