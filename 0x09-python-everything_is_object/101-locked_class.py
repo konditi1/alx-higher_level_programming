e15a982bfb517fe5a516a30386042f0f3e5f8fc7
konditi1 <konditi1@gmail.com>
2023-07-05 12:13:46 -0700

    added documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-	module for a class which is locked
+    module for a class which is locked
 """
 
 
 class LockedClass:
     """
-      class with a locked attribute first name
+        class with a locked attribute first name
     """
     __slots__ = "first_name"
