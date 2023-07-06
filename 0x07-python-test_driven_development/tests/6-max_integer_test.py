9cee214838ca9f3214dca12f48c7b1c0fd83e929
konditi1 <konditi1@gmail.com>
2023-07-06 11:43:37 -0700

    added import correctly

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,7 +5,8 @@ Test suite for the max_integer function
 
 
 import unittest
-max_integer = __import__('6-max_integer').max_integer
+from 6-max_integer import max_integer
+# max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
