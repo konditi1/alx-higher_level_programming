f1ebf6e1d0ea26f77c13bf41c076c8d5b3cfeed9
konditi1 <konditi1@gmail.com>
2023-07-07 09:26:50 -0700

    removed try excempt

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+""" Doc """
+
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-print(matrix_divided(matrix, 3))
-print(matrix)
+try:
+    matrix = [[3, 9], [12, 15]]
+    print(matrix_divided(matrix, 0))
+except Exception as e:
+    print(e)
