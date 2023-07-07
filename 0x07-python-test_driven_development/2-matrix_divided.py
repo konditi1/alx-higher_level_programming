df5c6da782bf4cfa243162d52b7a1d41a0bed71a
konditi1 <konditi1@gmail.com>
2023-07-07 09:30:11 -0700

    removed try excempt

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,8 +15,8 @@ def matrix_divided(matrix, div):
     msg = "matrix must be a matrix (list of lists) of integers/floats"
     msg1 = "Each row of the matrix must have the same size"
     if not isinstance(matrix, list) or not all(isinstance(row, list)
-                                                   for row in matrix):
-        raise TypeError (msg)
+                                               for row in matrix):
+        raise TypeError(msg)
     if len(matrix) == 0 or len(matrix[0]) == 0:
         raise ValueError("matrix cannot be empty")
     if not isinstance(div, (int, float)):
@@ -26,9 +26,9 @@ def matrix_divided(matrix, div):
     row_one_len = len(matrix[0])
     for row in matrix:
         if len(row) != row_one_len:
-            raise TypeError (msg1)
+            raise TypeError(msg1)
         if not all(isinstance(item, (int, float)) for item in row):
-            raise TypeError (msg)
+            raise TypeError(msg)
     updated_matrix = []
     for item in matrix:
         updated_row = [(round(element / div, 2)) for element in item]
