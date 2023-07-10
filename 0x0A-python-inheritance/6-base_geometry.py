fe92c84585a7c7fe5a80c893d1301a752ec27439
konditi1 <konditi1@gmail.com>
2023-07-10 14:27:48 -0700

    add a method erea BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    class called BaseGeometry
+    """
+    try:
+        def area(self):
+            raise Exception("area() is not implemented")
+        pass
+    except Exception as e:
+        pass
