6a0e7764517747c68ae69da68b4e5ef17c672798
konditi1 <konditi1@gmail.com>
2023-07-10 16:22:43 -0700

    implemented the passed area method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+"""
+class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    class called BaseGeometry
+    """
+    try:
+        def area(self):
+            """area() is not implemented"""
+            raise Exception("area() is not implemented")
+
+    except Exception as e:
+        pass
+
+    def integer_validator(self, name, value):
+        """integer_validator() is not implemented"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    class called Rectangle
+    attributes:
+        width: integer
+        height: integer
+    methods:
+        __init__(self, width, height)
+        area(self)
+        integer_validator(self, name, value)
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """:returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """:returns a string representation of the rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
