802c8fd8fbfeb9a0ae7e30e707e106745e7df151
konditi1 <konditi1@gmail.com>
2023-07-11 12:19:32 -0700

    saves an obtect to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+writes an object to a text file using json rep
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    function that writes an object to a text file using json rep
+    :param :
+        my_obj: object to be converted
+    :param :
+        filename: name of the file to be created
+    :return:
+    the filename of the created file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(json.dumps(my_obj))
