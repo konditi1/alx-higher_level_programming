7a615270c87a30351b752142bf2e56b9f1011fb5
konditi1 <konditi1@gmail.com>
2023-07-17 11:44:05 -0700

    corrected documentation for the square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,30 +1,32 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
 """
-import the Base class and Rectangle class
+Import the Base class and Rectangle class
 """
 from models.base import Base
 from models.rectangle import Rectangle
 
 """
-square class that inherits from Rectangle
+Square class that inherits from Rectangle
 """
 
 
 class Square(Rectangle):
     """
-    square class that inherits from Rectangle
+    Square class that inherits from Rectangle
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """
-        constructor for the square class
-        param:
-            size: size of the square
-            x: x-coordinate of the square
-            y: y-coordinate of the square
-            id: id of the object
-        return:
-            nothing
+        Constructor for the Square class.
+
+        Args:
+            size (int): Size of the square.
+            x (int): X-coordinate of the square's position. Default is 0.
+            y (int): Y-coordinate of the square's position. Default is 0.
+            id: ID of the object. Default is None.
+
+        Returns:
+            None
         """
         super().__init__(size, size, x, y, id)
         self.width = size
@@ -32,29 +34,36 @@ class Square(Rectangle):
 
     def __str__(self):
         """
-        returns a string representation of the square
+        Returns a string representation of the square.
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
 
     @property
     def size(self):
         """
-        getter method for the size attribute
+        Getter method for the size attribute.
         """
         return self.width
 
     @size.setter
     def size(self, value):
         """
-        setter method for the size attribute
+        Setter method for the size attribute.
         """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
         """
-        updates the attributes of the square
-        if *args exist and is not empty: **kwargs must be skipped
+        Updates the attributes of the square.
+        If *args exist and is not empty, **kwargs must be skipped.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            None
         """
         if len(args) == 0:
             for key, value in kwargs.items():
@@ -70,7 +79,7 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """
-        returns the dictionary representation of the square
+        Returns the dictionary representation of the square.
         """
         return {
             "id": self.id,
