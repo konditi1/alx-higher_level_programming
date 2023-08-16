32ab05374a3f479173cc41ac6c65fe0037212644
konditi1 <konditi1@gmail.com>
2023-08-16 22:13:16 +0300

    script that creates the MySQL server user user_0d_1.

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+--  script that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+-- grants all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost';
+-- flush privileges for user_0d_1
+FLUSH PRIVILEGES;
