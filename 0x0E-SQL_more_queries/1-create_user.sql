146c39523714b6fdcf992f7593a3f41d03f3bed8
konditi1 <konditi1@gmail.com>
2023-08-16 23:13:53 +0300

    script that creates the table id_not_null

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 --  script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED with mysql_native_password BY 'user_0d_1_pwd';
+IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
 -- grants all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.*
 TO 'user_0d_1'@'localhost';
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE IF NOT EXISTS force_name
+(
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
