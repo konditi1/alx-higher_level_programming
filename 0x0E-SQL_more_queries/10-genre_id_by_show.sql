a6648a8a1960e761597890912eee1f65f8f6b2cd
konditi1 <konditi1@gmail.com>
2023-08-17 21:22:04 +0300

    list all shows contained hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,13 @@
+--  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- use only one SELECT statement
+-- select the database
+USE hbtn_0d_tvshows;
+-- list all shows
+SELECT s.title, g.genre_id
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS g
+ON s.id = g.show_id
+ORDER BY s.title ASC, g.genre_id ASC;
+
