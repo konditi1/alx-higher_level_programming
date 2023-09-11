6d1beddef043a533725f28a992edcb304d79d126
konditi1 <konditi1@gmail.com>
2023-09-12 00:11:56 +0300

    13-add.js

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 // Define the add function
-function add(a, b) {
-    return a + b;
+function add (a, b) {
+  return a + b;
 }
 
 // Export the add function to make it visible from outside
