0eb1cb899d61a827cc445b98d194d46a1e4c2839
konditi1 <konditi1@gmail.com>
2023-09-12 20:15:50 +0300

    added check for h and h

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w <= 0 || h <= 0) {
-        return {};
-      }
-      this.width = w;
-      this.height = h;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return 'Rectangle ' + {};
     }
+    this.width = w;
+    this.height = h;
   }
-  module.exports = Rectangle;
-  
+}
+module.exports = Rectangle;
