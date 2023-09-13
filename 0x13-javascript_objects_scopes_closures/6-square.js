97a2590e477daa78006964ba8c71b2cd2835d6ca
konditi1 <konditi1@gmail.com>
2023-09-13 09:05:49 +0300

    implemented charPrint

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
     if (c === undefined) {
       this.print();
