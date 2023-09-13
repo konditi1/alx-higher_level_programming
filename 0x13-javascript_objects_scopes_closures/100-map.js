492663c2ad400ea4d572d8b6f85e47d26233de64
konditi1 <konditi1@gmail.com>
2023-09-13 15:39:11 +0300

    using map and index of in an array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data.js');
+const { list } = require('./100-data.js');
 const newList = list.map((x) => x * list.indexOf(x));
 console.log(list);
 console.log(newList);
