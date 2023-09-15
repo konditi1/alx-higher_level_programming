ee299b30978d933f7aa50932a69af1e182a1024b
konditi1 <konditi1@gmail.com>
2023-09-15 23:39:47 +0300

    getting all state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,34 +10,45 @@ def main():
     """
     List all states from the specified MySQL database.
     """
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username>"
+              " <mysql_password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
 
-    # Connect to the MySQL server
-    connection = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database_name
-    )
+    try:
+        # Connect to the MySQL server
+        connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name,
+            charset="utf8"
+        )
 
-    # Create a cursor object to interact with the database
-    cursor = connection.cursor()
+        # Create a cursor object to interact with the database
+        cursor = connection.cursor()
 
-    # Execute the SQL query to fetch states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        # Execute the SQL query to fetch states
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all the rows
-    states = cursor.fetchall()
+        # Fetch all the rows
+        states = cursor.fetchall()
 
-    # Display the results
-    for state in states:
-        print(state)
+        # Display the results
+        for state in states:
+            print(state)
 
-    cursor.close()
-    connection.close()
+    except MySQLdb.Error as e:
+        print("Error: {}".format(e))
+    finally:
+        # Close the cursor and the database connection
+        cursor.close()
+        connection.close()
 
 
 if __name__ == "__main__":
