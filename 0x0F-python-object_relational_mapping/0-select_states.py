3e5ee8e3f0a83f8e9ca517fb4f4d183aa2210167
konditi1 <konditi1@gmail.com>
2023-09-15 20:48:08 +0300

    script that lists all states from the database hbtn_0e_0_usa:

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ This script lists all states from the specified MySQL database.
 import MySQLdb
 import sys
 def list_states(username, password, database_name):
-        """
+"""
     List all states from the specified MySQL database.
 
     Args:
@@ -23,7 +23,7 @@ def list_states(username, password, database_name):
         MySQLdb.Error: If there's an error connecting to the database.
     """
     try:
-        # Connect to the MySQL server
+        """Connect to the MySQL server"""
         connection = MySQLdb.connect(
             host="localhost",
             port=3306,
@@ -33,23 +33,23 @@ def list_states(username, password, database_name):
             charset="utf8"
         )
 
-        # Create a cursor object to interact with the database
+        """Create a cursor object to interact with the database"""
         cursor = connection.cursor()
 
-        # Execute the SQL query to fetch states
+        """ Execute the SQL query to fetch states"""
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-        # Fetch all the rows
+        """Fetch all the rows"""
         states = cursor.fetchall()
 
-        # Display the results
+        """Display the results"""
         for state in states:
             print(state)
 
     except MySQLdb.Error as e:
         print("Error: {}".format(e))
     finally:
-        # Close the cursor and the database connection
+        """Close the cursor and the database connection"""
         if cursor:
             cursor.close()
         if connection:
