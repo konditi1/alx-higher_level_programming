57525a7609c71af319cf9167f28f182992b2f2c9
konditi1 <konditi1@gmail.com>
2023-09-15 21:30:14 +0300

    getting started

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,26 +4,22 @@ This script lists all states from the specified MySQL database.
 """
 import MySQLdb
 import sys
-def list_states(username, password, database_name):
-"""
-    List all states from the specified MySQL database.
 
-    Args:
-        username (str): The MySQL username.
-        password (str): The MySQL password.
-        database_name (str): The name of the database to connect to.
 
-    Returns:
-        None
-
-    Prints:
-        The list of states with their IDs and names.
-
-    Raises:
-        MySQLdb.Error: If there's an error connecting to the database.
+def main():
     """
+    List all states from the specified MySQL database.
+    """
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
     try:
-        """Connect to the MySQL server"""
+        # Connect to the MySQL server
         connection = MySQLdb.connect(
             host="localhost",
             port=3306,
@@ -33,35 +29,28 @@ def list_states(username, password, database_name):
             charset="utf8"
         )
 
-        """Create a cursor object to interact with the database"""
+        # Create a cursor object to interact with the database
         cursor = connection.cursor()
 
-        """ Execute the SQL query to fetch states"""
+        # Execute the SQL query to fetch states
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-        """Fetch all the rows"""
+        # Fetch all the rows
         states = cursor.fetchall()
 
-        """Display the results"""
+        # Display the results
         for state in states:
             print(state)
 
     except MySQLdb.Error as e:
         print("Error: {}".format(e))
     finally:
-        """Close the cursor and the database connection"""
+        # Close the cursor and the database connection
         if cursor:
             cursor.close()
         if connection:
             connection.close()
 
+
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    list_states(username, password, database_name)
+    main()
