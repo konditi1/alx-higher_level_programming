63156edba842930928122dd58bbaeab6dc1a5b37
konditi1 <konditi1@gmail.com>
2023-10-01 00:46:22 +0300

    script that fetches https://alx-intranet.hbtn.io/status and decodes

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 #  script that fetches https://alx-intranet.hbtn.io/status
-from urllib import request
+import requests
 # store the url in a variable
 url = "https://alx-intranet.hbtn.io/status"
 # open the url and read the response
 
-with request.urlopen(url) as response:
-    html1 = response.read()
-    html = html1.decode("utf-8")
+with requests.get(url) as response:
+    html = response.text
     print("Body response:")
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(html))
