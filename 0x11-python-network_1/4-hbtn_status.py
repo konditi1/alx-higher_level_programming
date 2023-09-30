a9e3cf0114569bcbed3261d68899cf2d0681f854
konditi1 <konditi1@gmail.com>
2023-10-01 00:42:24 +0300

    script that fetches https://alx-intranet.hbtn.io/status and decodes

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 #  script that fetches https://alx-intranet.hbtn.io/status
-from urllib import request, error
+from urllib import request
 # store the url in a variable
 url = "https://alx-intranet.hbtn.io/status"
 # open the url and read the response
-try:
-    with request.urlopen(url) as response:
-        html1 = response.read()
-        html = html1.decode("utf-8")
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-except error.HTTPError as error:
-    print("Error code: {}".format(error.code))
+
+with request.urlopen(url) as response:
+    html1 = response.read()
+    html = html1.decode("utf-8")
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
