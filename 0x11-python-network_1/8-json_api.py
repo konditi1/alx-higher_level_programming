bd9e92a93a6ab00c9ce40aa832cc48d8ea1be775
konditi1 <konditi1@gmail.com>
2023-10-01 21:43:22 +0300

    Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# script that takes a letter and sends a post request to http://0.0.0.0:5000/search_user
+# with the letter as a parameter
+import sys
+import requests
+
+# check if letter is provided as an argument
+if len(sys.argv) != 2 or not sys.argv[1].isalpha():
+    letter = ""
+    print("No request")
+    sys.exit(1)
+else:
+    letter = sys.argv[1]
+data = {'q': letter}
+# send a POST request to the passed URL with the letter as a parameter
+response = requests.post('http://0.0.0.0:5000/search_user', data=data)
+
+try:
+    # If the response body is properly JSON formatted and not empty
+    json_dict = response.json()
+    if json_dict == {}:
+        print("No result")
+    else:
+        print(f"[{json_dict['id']}] {json_dict['name']}")
+except ValueError:
+    print("Not a valid JSON")
