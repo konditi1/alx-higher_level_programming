1b25d1f4c5d48ce25496c000cd7b36091242f2fe
konditi1 <konditi1@gmail.com>
2023-10-01 15:22:31 +0300

    script that takes in a URL, sends a request to the URL and displays the body of the response.

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# script that sends a request to url and displays response body
+import sys
+import requests
+# check if url is provided as an argument
+if len(sys.argv) != 2:
+    print("Usage: {} <url>".format(sys.argv[0]))
+    sys.exit()
+# get the url
+url = sys.argv[1]
+# open the url and read the response
+try:
+    with requests.get(url) as response:
+        html = response.text
+        print(html)
+except requests.exceptions.HTTPError as error:
+    print("Error code: {}".format(error.code))
