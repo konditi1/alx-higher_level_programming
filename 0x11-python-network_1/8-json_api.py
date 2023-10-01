a3456da21c4023d5bf9eb7d920331e4ee8787a3d
konditi1 <konditi1@gmail.com>
2023-10-01 22:43:15 +0300

    Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-# script that takes a letter and sends a post request to url
-# with the letter as a parameter
+"""
+Search API
+"""
 import sys
 import requests
 
-# check if more than one argument is provided
-if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        letter = ""
-    else:
-        letter = sys.argv[1]
-    data = {'q': letter}
-    # send a POST request to the passed URL with the letter as a parameter
-    response = requests.post('http://0.0.0.0:5000/search_user', data=data)
 
+if __name__ == "__main__":
+    alpha = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": alpha}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        # If the response body is properly JSON formatted and not empty
-        json_dict = response.json()
-        if json_dict == {}:
+        response = r.json()
+        if response == {}:
             print("No result")
         else:
-            print(f"[{json_dict['id']}] {json_dict['name']}")
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
