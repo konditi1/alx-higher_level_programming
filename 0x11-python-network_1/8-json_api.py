cb89dbfb0188ef262b1c0661edc9aef74abed492
konditi1 <konditi1@gmail.com>
2023-10-01 22:21:01 +0300

    Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,22 +4,23 @@
 import sys
 import requests
 
-if __name__ == "__main__":
-    # check if letter is provided as an argument
-    if len(sys.argv) != 1:
-        letter = ""
-    else:
-        letter = sys.argv[1]
-    data = {'q': letter}
-    # send a POST request to the passed URL with the letter as a parameter
-    response = requests.post('http://0.0.0.0:5000/search_user', data=data)
+# check if letter is provided as an argument
+if len(sys.argv) != 2 or not sys.argv[1].isalpha():
+    letter = ""
+    print("No request")
+    sys.exit(1)
+else:
+    letter = sys.argv[1]
+data = {'q': letter}
+# send a POST request to the passed URL with the letter as a parameter
+response = requests.post('http://0.0.0.0:5000/search_user', data=data)
 
-    try:
-        # If the response body is properly JSON formatted
-        json_dict = response.json()
-        if json_dict == {}:
-            print("No result")
-        else:
-            print(f"[{json_dict['id']}] {json_dict['name']}")
-    except ValueError:
-        print("Not a valid JSON")
+try:
+    # If the response body is properly JSON formatted and not empty
+    json_dict = response.json()
+    if json_dict == {}:
+        print("No result")
+    else:
+        print(f"[{json_dict['id']}] {json_dict['name']}")
+except ValueError:
+    print("Not a valid JSON")
